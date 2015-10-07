f9f9b3cbbbb5ec0c2d778a744729eb39748e189a
bochatayy <yannick.bochatay@meteo.fr>
2015-10-07 17:05:07 +0200

    Commit initial.

---
+++ tests/tests.js
@@ -0,0 +1,30 @@
+if (typeof require!= "undefined") {
+    
+    require.config({
+        paths: {
+            "jsyg-path": '../JSYG.Path',
+            "jquery":"../bower_components/jquery/dist/jquery",
+            "jsyg":"../bower_components/jsyg/JSYG"
+        },
+        urlArgs: "bust=" + (+new Date())
+    });
+}
+
+(function(factory) {
+    
+    if (typeof define == 'function' && define.amd) define(["jsyg-path"],factory);
+    else factory(JSYG.Path);
+    
+}(function(Path) {
+
+    module("JSYG.Path");
+
+    test("Création d'un chemin", function() {     
+
+        var path = new Path();
+        path.moveTo(0,0).lineTo(30,50).lineTo(80,80);
+        
+        equal( path.nbSegs(), 3 ,"nombre de segments");
+    });
+    
+}));
